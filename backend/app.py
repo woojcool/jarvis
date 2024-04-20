5a0039af81a6a5c4cc64bf8fef3a99b97a1330cb
Arush <akarushkumar7@gmail.com>
2024-04-20 17:10:29 -0400

    test route python

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 from blueprints.auth import auth
 from blueprints.habits import habits
 from blueprints.tasks import tasks
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(auth)
 app.register_blueprint(habits)
 app.register_blueprint(tasks)
+
+# just to ensure the api is up and running
+@app.route('/test', methods=['GET'])
+def test():
+    return "App is up and running", 201
 
 
 if __name__ == '__main__':
