b655546ad03dab3e313552ac8a634beacc4da52a
Woojune <woojune@Woojunes-MacBook-Pro-2.local>
2024-04-20 15:08:54 -0400

    pete check

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from blueprints.auth import auth
 from blueprints.habits import habits
 from blueprints.tasks import tasks
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.register_blueprint(auth)
 app.register_blueprint(habits)
 app.register_blueprint(tasks)
+bcrypt = Bcrypt()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
