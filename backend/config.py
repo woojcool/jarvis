ffb36a83ff916ed4aa0dabce37cb2609c3471d2d
Woojune <woojune@Woojunes-MacBook-Pro-2.local>
2024-04-20 15:03:10 -0400

    fixed config

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = b'G${-[\xe3G\x04K/z\x90\\vV\x1f' ## updated secret key (4/20)
-MONGODB_HOST = 'mongodb+srv://woojcool:cOfOaxg4HzM80RHf@cluster0.etdhxjc.mongodb.net/reviews?retryWrites=true&w=majority&appName=Cluster0'
+MONGODB_HOST = 'mongodb+srv://woojcool:uMUKqW4pdjuN6PSM@cluster0.etdhxjc.mongodb.net/'
