c99947360ee883805ee91242fc45cf94e6015637
woojcool <134987234+woojcool@users.noreply.github.com>
2024-04-20 21:03:32 -0400

    Update tasks.py

--- backend/blueprints/tasks.py
+++ backend/blueprints/tasks.py
@@ -2,19 +2,37 @@ from flask import Blueprint, request
 from database import users, tasks
 from bson.objectid import ObjectId
 
-
 tasks_bp = Blueprint("tasks", __name__, url_prefix='/tasks')
 
-@tasks_bp.route('/', methods=['POST'])
+@tasks_bp.route('', methods=['POST'])
 def create_task():
     userID = request.headers.get('Authorization')
     user = users.find_one({"_id": ObjectId(userID)})
     if not user:
         return {'error': 'User not found'}, 404
+    
+    body = request.get_json(force=True)
+    name = body['name']
+    deadline = body['deadline']
 
-    return
+    new_task = {
+        "_userID": ObjectId(userID),
+        "name": name,
+        "deadline": deadline,
+        "completed": False
+    }
+    result = tasks.insert_one(new_task)
+    new_task = {
+        "taskID": str(result.inserted_id),
+        "name": name,
+        "deadline": deadline,
+        "completed": False
+    }
 
-@tasks_bp.route('/', methods=['GET'])
+    return new_task, 201
+
+
+@tasks_bp.route('', methods=['GET'])
 def get_all_tasks():
     userID = request.headers.get('Authorization')
     user = users.find_one({"_id": ObjectId(userID)})
