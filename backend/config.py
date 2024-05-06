1b943e8108c2ee7a199749401890f08e65b855bb
woojcool <134987234+woojcool@users.noreply.github.com>
2024-05-05 22:59:28 -0400

    back to keys

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,2 @@
-import os #Now need to import os
-SECRET_KEY = os.environ.get('SECRET_KEY')
-MONGODB_HOST = os.environ.get('MONGODB_HOST')
+MONGODB_HOST = 'mongodb+srv://pee:poo@cluster0.095x1re.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+SECRET_SALT = b'$2b$12$weRXOZ7rCfsu/deAmd3H0u'
