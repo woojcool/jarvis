194b8b8ed2214a46b6cf8619f16ac642220a8cf9
woojcool <134987234+woojcool@users.noreply.github.com>
2024-05-06 00:26:38 -0400

    Update _layout.tsx

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -24,7 +24,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "/",
+  initialRouteName: "index",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
